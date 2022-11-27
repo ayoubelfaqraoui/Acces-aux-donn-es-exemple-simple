8c609ea1be52f43d611bdb208d72e58b391307c5
ayoubelfaqraoui <116494488+ayoubelfaqraoui@users.noreply.github.com>
2022-11-27 04:12:42 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.txtId_dossier = new System.Windows.Forms.TextBox();
+            this.txtNom_dossier = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.validate = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtType_dossier = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 33);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(377, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(39, 332);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 1;
+            this.btnInsert.Text = "insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // txtId_dossier
+            // 
+            this.txtId_dossier.Location = new System.Drawing.Point(49, 217);
+            this.txtId_dossier.Name = "txtId_dossier";
+            this.txtId_dossier.Size = new System.Drawing.Size(100, 20);
+            this.txtId_dossier.TabIndex = 2;
+            // 
+            // txtNom_dossier
+            // 
+            this.txtNom_dossier.Location = new System.Drawing.Point(49, 243);
+            this.txtNom_dossier.Name = "txtNom_dossier";
+            this.txtNom_dossier.Size = new System.Drawing.Size(100, 20);
+            this.txtNom_dossier.TabIndex = 3;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(120, 332);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(201, 332);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(419, 33);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "select";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // validate
+            // 
+            this.validate.Location = new System.Drawing.Point(293, 332);
+            this.validate.Name = "validate";
+            this.validate.Size = new System.Drawing.Size(75, 23);
+            this.validate.TabIndex = 8;
+            this.validate.Text = "valiadte";
+            this.validate.UseVisualStyleBackColor = true;
+            this.validate.Click += new System.EventHandler(this.validate_Click);
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(396, 332);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 9;
+            this.cancel.Text = "cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(132, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // txtType_dossier
+            // 
+            this.txtType_dossier.Location = new System.Drawing.Point(49, 269);
+            this.txtType_dossier.Name = "txtType_dossier";
+            this.txtType_dossier.Size = new System.Drawing.Size(100, 20);
+            this.txtType_dossier.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 380);
+            this.Controls.Add(this.txtType_dossier);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.validate);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtNom_dossier);
+            this.Controls.Add(this.txtId_dossier);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.TextBox txtId_dossier;
+        private System.Windows.Forms.TextBox txtNom_dossier;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button validate;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtType_dossier;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    { private int value;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        ////Pour Access----  using System.Data.OleDb;
+        //OleDbConnection AccesConnection = new OleDbConnection();
+        //OleDbDataAdapter AccesAdapter = new OleDbDataAdapter();
+        //String AccesChaineConnection = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source=|DataDirectory|\\BDAcces.mdb;Persist Security Info=True";
+
+        ////Pour SQL Server----using System.Data.SqlClient;
+
+
+        ////Pour MySQL-----using MySql.Data.MySqlClient;
+        //MySqlConnection MySQLConnection = new MySqlConnection();
+        //MySqlDataAdapter MySQLAdapter = new MySqlDataAdapter();
+        //String MySQLChaineConnection = "server=localhost; user id=root;password= ;database=BdMySQL";
+
+        ////Pour Oracle---------using System.Data.OracleClient;
+        //OracleConnection OracleConnection = new OracleConnection();
+        //OracleDataAdapter OracleAdapter = new OracleDataAdapter();
+        //String OracleChaineConnection = "Data Source=BdOracle;User Id=hr;Password=hr";//;DBA Privilege=SYSDBA
+
+
+
+        static string chaine = @"Data Source=DESKTOP-PQOLN1N\SQLEXPRESS;Initial Catalog=mydatabase;Integrated Security=True";
+        //"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\App_Data\VotreBD.mdf;Integrated Security=True;User Instance=True"
+        //"Server=.\SQLEXPRESS; DataBase=VotreBD;USER ID=sa; PASSWORD="
+        static SqlConnection cnx = new SqlConnection(chaine);
+        static SqlCommand cmd = new SqlCommand();
+        static SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button4.Enabled = true;
+            btnInsert.Enabled = true;
+            btnUpdate.Enabled = true;
+            btnDelete.Enabled = true;
+            validate.Enabled=false;
+            cancel.Enabled = false;
+            txtId_dossier.Enabled = false;
+            txtNom_dossier.Enabled=false;
+            txtType_dossier.Enabled=false ;
+            comboBox1.Enabled = false;
+
+        }
+
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            cnx.Open();
+            cmd.Connection = cnx;
+  
+            cnx.Close();
+            btnInsert.Enabled=false;
+            btnUpdate.Enabled=false;
+            btnDelete.Enabled=false;
+            validate.Enabled= true;
+            cancel.Enabled=true;
+            button4.Enabled = false;
+            txtNom_dossier.Enabled=true;
+            txtType_dossier.Enabled=true;
+            txtId_dossier.Enabled=true ;
+            txtNom_dossier.Clear();
+            txtId_dossier.Clear();
+            txtType_dossier.Clear();
+
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            value = 2;
+            cnx.Open();
+            cmd.Connection = cnx;
+
+          
+            btnInsert.Enabled = false;
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+            validate.Enabled = true;
+            cancel.Enabled = true;
+            button4.Enabled = false;
+            txtNom_dossier.Enabled = true;
+            txtType_dossier.Enabled = true;
+            txtId_dossier.Enabled = true;
+            txtNom_dossier.Clear();
+            txtId_dossier.Clear();
+            txtType_dossier.Clear();
+            cnx.Close();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {  value=3;
+            cnx.Open();
+            cmd.Connection = cnx;
+            btnInsert.Enabled = false;
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+            validate.Enabled = true;
+            cancel.Enabled = true;
+            button4.Enabled = false;
+            txtNom_dossier.Enabled = true;
+            txtType_dossier.Enabled = true;
+            txtId_dossier.Enabled = true;
+            txtNom_dossier.Clear();
+            txtId_dossier.Clear();
+            txtType_dossier.Clear();
+            cnx.Close();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            value = 1;
+            cnx.Open();
+            cmd.CommandText = "select * from movie";
+            cmd.Connection = cnx;
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            comboBox1.DataSource = dt;
+            button4.Enabled = false;
+            validate.Enabled = false;
+            cancel.Enabled=false;
+            txtId_dossier.Enabled = false;
+            txtNom_dossier.Enabled = false;
+            txtType_dossier.Enabled = false;
+            comboBox1.Enabled = true;
+            comboBox1.DisplayMember = "nom";
+            comboBox1.ValueMember = "id_movie";
+            txtNom_dossier.DataBindings.Clear();
+            txtId_dossier.DataBindings.Clear();
+            txtType_dossier.DataBindings.Clear();
+            txtNom_dossier.DataBindings.Add("text",comboBox1.DataSource,"nom");
+            txtId_dossier.DataBindings.Add("text", comboBox1.DataSource, "id_movie"); ;
+            txtType_dossier.DataBindings.Add("text", comboBox1.DataSource, "type");
+            cnx.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void validate_Click(object sender, EventArgs e)
+        {
+            if (value == 1) {
+                cnx.Open();
+                cmd.Connection = cnx;
+                if (txtId_dossier.Text == "" || txtNom_dossier.Text == "" || txtType_dossier.Text == "")
+                { MessageBox.Show("motherfucker enter the inputs"); }
+                else
+                {
+                    cmd.CommandText = "insert into movie(id_movie, nom,type) values('" + txtId_dossier.Text + "','" + txtNom_dossier.Text + "','" + txtType_dossier.Text + "') ";
+                    cmd.ExecuteNonQuery();
+                    button4.Enabled = true;
+                    btnInsert.Enabled = true;
+                    btnUpdate.Enabled = true;
+                    btnDelete.Enabled = true;
+                    validate.Enabled = false;
+                    cancel.Enabled = false;
+                    txtId_dossier.Enabled = false;
+                    txtNom_dossier.Enabled = false;
+                    txtType_dossier.Enabled = false;
+                    comboBox1.Enabled = false;
+                }
+            }
+            else if (value == 2)
+            {
+                if (txtId_dossier.Text == "" || txtNom_dossier.Text == "" || txtType_dossier.Text == "")
+                { MessageBox.Show("motherfucker enter the inputs"); }
+                else
+                {
+                    cnx.Open();
+                    cmd.Connection = cnx;
+                    cmd.CommandText = "update movie set nom ='" + txtNom_dossier.Text + "', type ='" + txtType_dossier.Text + "' where id_movie='" + txtId_dossier.Text + "' ";
+                    cmd.ExecuteNonQuery();
+                    button4.Enabled = true;
+                    btnInsert.Enabled = true;
+                    btnUpdate.Enabled = true;
+                    btnDelete.Enabled = true;
+                    validate.Enabled = false;
+                    cancel.Enabled = false;
+                    txtId_dossier.Enabled = false;
+                    txtNom_dossier.Enabled = false;
+                    txtType_dossier.Enabled = false;
+                    comboBox1.Enabled = false;
+                    cnx.Close();
+                }
+            }
+            else if (value == 3)
+            {
+                if (txtId_dossier.Text == "" || txtNom_dossier.Text == "" || txtType_dossier.Text == "")
+                { MessageBox.Show("motherfucker enter the inputs"); }
+                else
+                {
+                    cnx.Open();
+                    cmd.Connection = cnx;
+                    cmd.CommandText = "delete from movie where id_movie='" + txtId_dossier.Text + "' ";
+                    cmd.ExecuteNonQuery();
+                    button4.Enabled = true;
+                    btnInsert.Enabled = true;
+                    btnUpdate.Enabled = true;
+                    btnDelete.Enabled = true;
+                    validate.Enabled = false;
+                    cancel.Enabled = false;
+                    txtId_dossier.Enabled = false;
+                    txtNom_dossier.Enabled = false;
+                    txtType_dossier.Enabled = false;
+                    comboBox1.Enabled = false;
+                    cnx.Close();
+                }
+            }
+
+
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            button4.Enabled = true;
+            btnInsert.Enabled = true;
+            btnUpdate.Enabled = true;
+            btnDelete.Enabled = true;
+            validate.Enabled = false;
+            cancel.Enabled = false;
+            txtId_dossier.Enabled = false;
+            txtNom_dossier.Enabled = false;
+            txtType_dossier.Enabled = false;
+            comboBox1.Enabled = false;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
